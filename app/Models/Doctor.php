51c8066d29f739b243988ca103e5b1f82a9fa82c
aghazal <ali.ghazal@robustastudio.com>
2021-01-22 08:58:29 +0200

    models

---
+++ app/Models/Doctor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function path() {
+        return '/doctor/' . $this->id;
+    }
+
+    public function hospitals(){
+        return $this->belongsToMany(Hospital::class);
+    }
+}
---
+++ app/Models/Hospital.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hospital extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function path()
+    {
+        return '/hospital/' . $this->id;
+    }
+
+    public function doctors(){
+        return $this->belongsToMany(Doctor::class);
+    }
+
+}
