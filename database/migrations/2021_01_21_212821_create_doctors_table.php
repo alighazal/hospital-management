73294ca55cbb2ea786f1c3a16bf99fe51a4e318d
aghazal <ali.ghazal@robustastudio.com>
2021-01-22 08:59:10 +0200

    factories & migrtions & seeders

---
+++ database/migrations/2021_01_21_212821_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("speciality");
+            $table->string("seniority");
+            $table->string("alma_mater");
+            $table->timestamp("dob");
+            $table->timestamp("graduation_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
